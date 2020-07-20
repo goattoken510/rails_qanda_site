07ce122b9b3af5101fc76c81fa6168a9b29dc93a
Kenta Goto <goattoken510@gmail.com>
2020-07-20 12:48:03 +0000

    create answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_111250) do
+ActiveRecord::Schema.define(version: 2020_07_20_124433) do
 
-  create_table "questions", force: :cascade do |t|
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
     t.string "name"
-    t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "quistions", force: :cascade do |t|
+  create_table "questions", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.text "content"
